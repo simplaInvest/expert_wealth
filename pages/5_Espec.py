06845608e664ca53a24e57c7b75bd8fd7a856278
Sávio Trindade <122768191+savio-810@users.noreply.github.com>
2025-02-04 11:32:13 -0300

    tentando consertar a viz de espectador

--- pages/5_Espec.py
+++ pages/5_Espec.py
@@ -12,18 +12,16 @@ import time
 from sidebar import setup_sidebar
 from funcs import load_calls, format_data, get_last_30_days_data
 
-st.set_page_config(page_title="Espectador", page_icon="👀", layout = 'wide')
+st.set_page_config(page_title="Espectador", page_icon="👀", layout='wide')
 
-# Se o usuário estiver logado, exibe a sidebar
-# Chama a sidebar
+# Configurar sidebar
 setup_sidebar()
-
 
 st.title("Modo Espectador")
 st.write("Bem-vindo ao modo espectador do Dashboard.")
 
 ###########################################################################################################
-#                                Carregar dados
+# Carregar dados
 
 if st.button("Limpar Cache"):
     st.cache_data.clear()
@@ -32,171 +30,138 @@ if st.button("Limpar Cache"):
 
 filtered_data = get_last_30_days_data()
 
-st.dataframe(filtered_data)
-
 ###########################################################################################################
 
 col1, col2 = st.columns(2)
 
 with col1:
     with st.container(border=True):
-        from datetime import datetime, timedelta
-        import plotly.express as px
-        import streamlit as st
-
-        # Obter data de hoje e amanhã
         today_start = datetime.now().replace(hour=0, minute=0, second=1, microsecond=0)  # Hoje 00:00:01
         tomorrow_start = today_start + timedelta(days=1)  # Amanhã 00:00:01
 
-        # Criar subdataset com chamadas feitas hoje
+        # Filtrar dados de hoje
         filtered_data_today = filtered_data[
             (filtered_data['connect_time'] >= today_start) & 
             (filtered_data['connect_time'] < tomorrow_start)
         ]
 
-        # Contagem de valores e ordenação
-        sdr_counts = filtered_data_today['CLI'].value_counts().sort_values(ascending=True)
+        # Contagem de SDRs
+        sdr_counts = filtered_data_today['CLI'].value_counts().sort_values(ascending=True).reset_index()
+        sdr_counts.columns = ['SDR', 'Número de Ligações']
 
-        # Criar o gráfico de barras com os valores exibidos em cada barra
-        bar_fig = px.bar(
-            sdr_counts, 
-            orientation='h', 
-            title='Ligações hoje', 
-            labels={'index': 'SDR', 'value': 'Número de Ligações'},
-            text=sdr_counts  # Adiciona os valores das barras
-        )
+        if not sdr_counts.empty:
+            # Criar gráfico
+            bar_fig = px.bar(
+                sdr_counts, 
+                x='Número de Ligações',
+                y='SDR',
+                orientation='h', 
+                title='Ligações hoje',
+                text='Número de Ligações'
+            )
 
-        # Calcular a média das ligações por SDR
-        avg_calls = sdr_counts.mean()
+            # Adicionar linha média
+            avg_calls = sdr_counts["Número de Ligações"].mean()
+            bar_fig.add_shape(
+                type='line',
+                x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
+                line=dict(color='red', width=2, dash='dot')
+            )
+            bar_fig.add_annotation(
+                x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
+                showarrow=True, arrowhead=2, ax=0, ay=-40, bgcolor="red"
+            )
 
-        # Adicionar linha vertical pontilhada vermelha para a média
-        bar_fig.add_shape(
-            type='line',
-            x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
-            line=dict(color='red', width=2, dash='dot')
-        )
+            # Exibir gráfico
+            st.plotly_chart(bar_fig, use_container_width=True)
+        else:
+            st.warning("Nenhum dado disponível para exibição no gráfico.")
 
-        # Adicionar anotação para o valor da média no eixo x
-        bar_fig.add_annotation(
-            x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
-            showarrow=True, arrowhead=2, ax=0, ay=-40,
-            bgcolor="red"
-        )
-
-        # Ajustar o formato do texto nas barras
-        bar_fig.update_traces(textposition='outside')  # Posição do texto fora das barras
-
-        # Exibir o gráfico
-        st.plotly_chart(bar_fig, use_container_width=True, key="chart_today")
-
-    
     with st.container(border=True):
-        # Obter contagem de valores e ordenar em ordem decrescente
-        sdr_counts = filtered_data['CLI'].value_counts().sort_values(ascending=True)
-        
-        # Criar o gráfico de barras com os valores exibidos em cada barra
-        bar_fig = px.bar(
-            sdr_counts, 
-            orientation='h', 
-            title='Ligações nos últimos 30 dias', 
-            labels={'index': 'SDR', 'value': 'Número de Ligações'},
-            text=sdr_counts  # Adiciona os valores das barras
-        )
-        
-        # Calcular a média das ligações por SDR
-        avg_calls = sdr_counts.mean()
-        
-        # Adicionar linha vertical pontilhada vermelha para a média
-        bar_fig.add_shape(
-            type='line',
-            x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
-            line=dict(color='red', width=2, dash='dot')
-        )
-        
-        # Adicionar anotação para o valor da média no eixo x
-        bar_fig.add_annotation(
-            x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
-            showarrow=True, arrowhead=2, ax=0, ay=-40,
-            bgcolor="red"
-        )
-        
-        # Ajustar o formato do texto nas barras
-        bar_fig.update_traces(textposition='outside')  # Posição do texto fora das barras
-        
-        # Exibir o gráfico
-        st.plotly_chart(bar_fig, use_container_width=True, key="chart_30_days")
+        sdr_counts = filtered_data['CLI'].value_counts().sort_values(ascending=True).reset_index()
+        sdr_counts.columns = ['SDR', 'Número de Ligações']
+
+        if not sdr_counts.empty:
+            bar_fig = px.bar(
+                sdr_counts, 
+                x='Número de Ligações',
+                y='SDR',
+                orientation='h', 
+                title='Ligações nos últimos 30 dias',
+                text='Número de Ligações'
+            )
+            avg_calls = sdr_counts["Número de Ligações"].mean()
+            bar_fig.add_shape(
+                type='line',
+                x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
+                line=dict(color='red', width=2, dash='dot')
+            )
+            bar_fig.add_annotation(
+                x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
+                showarrow=True, arrowhead=2, ax=0, ay=-40, bgcolor="red"
+            )
+
+            st.plotly_chart(bar_fig, use_container_width=True)
+        else:
+            st.warning("Nenhum dado disponível para exibição no gráfico.")
 
 with col2:
-    filtered_data_1_min = filtered_data_today[filtered_data['call_time'] >= timedelta(minutes=1)]
+    filtered_data_1_min = filtered_data_today[filtered_data_today['call_time'] >= timedelta(minutes=1)]
     with st.container(border=True):
-        # Contagem de valores e ordenação
-        sdr_counts = filtered_data_1_min['CLI'].value_counts().sort_values(ascending=True)
+        sdr_counts = filtered_data_1_min['CLI'].value_counts().sort_values(ascending=True).reset_index()
+        sdr_counts.columns = ['SDR', 'Número de Ligações']
 
-        # Criar o gráfico de barras com os valores exibidos em cada barra
-        bar_fig = px.bar(
-            sdr_counts, 
-            orientation='h', 
-            title='Ligações atendidas hoje', 
-            labels={'index': 'SDR', 'value': 'Número de Ligações'},
-            text=sdr_counts  # Adiciona os valores das barras
-        )
+        if not sdr_counts.empty:
+            bar_fig = px.bar(
+                sdr_counts, 
+                x='Número de Ligações',
+                y='SDR',
+                orientation='h', 
+                title='Ligações atendidas hoje',
+                text='Número de Ligações'
+            )
 
-        # Calcular a média das ligações por SDR
-        avg_calls = sdr_counts.mean()
+            avg_calls = sdr_counts["Número de Ligações"].mean()
+            bar_fig.add_shape(
+                type='line',
+                x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
+                line=dict(color='red', width=2, dash='dot')
+            )
+            bar_fig.add_annotation(
+                x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
+                showarrow=True, arrowhead=2, ax=0, ay=-40, bgcolor="red"
+            )
 
-        # Adicionar linha vertical pontilhada vermelha para a média
-        bar_fig.add_shape(
-            type='line',
-            x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
-            line=dict(color='red', width=2, dash='dot')
-        )
+            st.plotly_chart(bar_fig, use_container_width=True)
+        else:
+            st.warning("Nenhum dado disponível para exibição no gráfico.")
 
-        # Adicionar anotação para o valor da média no eixo x
-        bar_fig.add_annotation(
-            x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
-            showarrow=True, arrowhead=2, ax=0, ay=-40,
-            bgcolor="red"
-        )
-
-        # Ajustar o formato do texto nas barras
-        bar_fig.update_traces(textposition='outside')  # Posição do texto fora das barras
-
-        # Exibir o gráfico
-        st.plotly_chart(bar_fig, use_container_width=True, key="chart_today_1_min")
-
-    filtered_data_1_min_30days = filtered_data[filtered_data['call_time'] >= timedelta(minutes=1)]
+    filtered_data_1_min_30days = filtered_data.loc[filtered_data['call_time'] >= timedelta(minutes=1)]
     with st.container(border=True):
-        # Contagem de valores e ordenação
-        sdr_counts = filtered_data_1_min_30days['CLI'].value_counts().sort_values(ascending=True)
+        sdr_counts = filtered_data_1_min_30days['CLI'].value_counts().sort_values(ascending=True).reset_index()
+        sdr_counts.columns = ['SDR', 'Número de Ligações']
 
-        # Criar o gráfico de barras com os valores exibidos em cada barra
-        bar_fig = px.bar(
-            sdr_counts, 
-            orientation='h', 
-            title='Ligações atendidas nos últimos 30 dias', 
-            labels={'index': 'SDR', 'value': 'Número de Ligações'},
-            text=sdr_counts  # Adiciona os valores das barras
-        )
+        if not sdr_counts.empty:
+            bar_fig = px.bar(
+                sdr_counts, 
+                x='Número de Ligações',
+                y='SDR',
+                orientation='h', 
+                title='Ligações atendidas nos últimos 30 dias',
+                text='Número de Ligações'
+            )
 
-        # Calcular a média das ligações por SDR
-        avg_calls = sdr_counts.mean()
+            avg_calls = sdr_counts["Número de Ligações"].mean()
+            bar_fig.add_shape(
+                type='line',
+                x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
+                line=dict(color='red', width=2, dash='dot')
+            )
+            bar_fig.add_annotation(
+                x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
+                showarrow=True, arrowhead=2, ax=0, ay=-40, bgcolor="red"
+            )
 
-        # Adicionar linha vertical pontilhada vermelha para a média
-        bar_fig.add_shape(
-            type='line',
-            x0=avg_calls, y0=-0.5, x1=avg_calls, y1=len(sdr_counts) - 0.5,
-            line=dict(color='red', width=2, dash='dot')
-        )
-
-        # Adicionar anotação para o valor da média no eixo x
-        bar_fig.add_annotation(
-            x=avg_calls, y=len(sdr_counts) - 0.5, text=f"Média: {avg_calls:.2f}",
-            showarrow=True, arrowhead=2, ax=0, ay=-40,
-            bgcolor="red"
-        )
-
-        # Ajustar o formato do texto nas barras
-        bar_fig.update_traces(textposition='outside')  # Posição do texto fora das barras
-
-        # Exibir o gráfico
-        st.plotly_chart(bar_fig, use_container_width=True, key="chart_30days_1_min")
+            st.plotly_chart(bar_fig, use_container_width=True)
+        else:
+            st.warning("Nenhum dado disponível para exibição no gráfico.")
