369e6ddb6da32f606f1f41fc8c1ff7d962e1a7e5
Sávio Trindade <122768191+savio-810@users.noreply.github.com>
2025-07-04 12:35:47 -0300

    minibug

--- pages/2_SDR.py
+++ pages/2_SDR.py
@@ -18,7 +18,6 @@ import matplotlib.colors as mcolors
 import random
 from datetime import datetime, timedelta
 import re
-from streamlit_option_menu import option_menu
 
 from funcs import carregar_dataframes, pag_sdr
 from sidebar import setup_sidebar
